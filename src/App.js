30f6d32e22458ab46b5d24d5a436388ce1291a97
Grace <hello@localhost>
2021-07-25 19:01:20 +0800

    determine day or night

--- src/App.js
+++ src/App.js
@@ -14,13 +14,50 @@ function fetchCurrentObservation() {
     const weatherTemp = weatherInfo?.weatherElement?.find(e => e.elementName === "TEMP");
     const weatherWind = weatherInfo?.weatherElement?.find(e => e.elementName === "WDSD");
     const weatherHumd = weatherInfo?.weatherElement?.find(e => e.elementName === "HUMD");
-
     return {
       location: weatherInfo?.locationName,
       temperature: weatherTemp?.elementValue,
       windSpeed: weatherWind?.elementValue,
       humidity: weatherHumd?.elementValue,
     };
+  });
+}
+
+function fetchCurrentWeatherType() {
+  return fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${process.env.REACT_APP_CWB_API_KEY}&format=JSON&locationName=臺北市`).then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const weatherRecords = data.records?.location?.find(e => e.locationName === '臺北市');
+    const weatherElement = weatherRecords?.weatherElement?.find(e => e.elementName === 'Wx');
+    const weatherDes = weatherElement?.time?.[0].parameter?.parameterName;
+    const weatherType = weatherElement?.time?.[0].parameter?.parameterValue;
+    return {
+      description: weatherDes,
+      type: weatherType,
+    };
+  });
+}
+
+function fetchSunriseSunset() {
+  const now = new Date();
+  const nowString = yearMonthDayString(now);
+  const tomorrow = addDays(now, 1);
+  const tomorrowString = yearMonthDayString(tomorrow);
+
+  return fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=${process.env.REACT_APP_CWB_API_KEY}&format=JSON&locationName=%E8%87%BA%E5%8C%97%E5%B8%82&timeFrom=${nowString}&timeTo=${tomorrowString}`).then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const sunriseSunsetData = data.records?.locations?.location?.find(e => e.locationName ==='臺北市');
+    const today = sunriseSunsetData?.time?.find(e => e.dataTime === nowString);
+    const sunrise = today?.parameter?.find(e => e.parameterName  === '日出時刻');
+    const sunriseTime = sunrise?.parameterValue;
+    const sunset = today?.parameter?.find(e => e.parameterName  === '日沒時刻');
+    const sunsetTime = sunset?.parameterValue;
+    const sunriseDateAndTime = new Date(`${nowString}T${sunriseTime}+08:00`);
+    const sunsetDateAndTime = new Date(`${nowString}T${sunsetTime}+08:00`);
+    return isDay(now, sunriseDateAndTime, sunsetDateAndTime)
   });
 }
 
@@ -32,30 +69,30 @@ function Dashboard() {
     humidity:'',
   });
 
+  const [weatherType, setWeatherType] = useState({
+    description: '',
+    type: '',
+  });
+
+  const [isDay, setIsDay] = useState(true);
+
   const handleClick = () => {
     Promise.all([
-      fetchCurrentObservation()
-    ]).then(([currentObservation]) =>{
-
+      fetchCurrentObservation(),fetchCurrentWeatherType(),fetchSunriseSunset()
+    ]).then(([currentObservation, currentWeatherType, sunriseSunset]) =>{
         setObservation(currentObservation);
+        setWeatherType(currentWeatherType);
+        setIsDay(sunriseSunset);
       });
-
   }
-
-  // const [weatherType, setWeatherType] = useState({
-  //   description:'',
-  //   type:'',
-  // });
-
-  // const [forecast, setForecast] = useState([]);
 
   return (
     <div className="dashboard">
       <button className="button" onClick={handleClick}></button>
       <div className="weatherObservation">
-        <Nowcasting observation={observation}/>
-        {/* <WeatherIcon />
-        <Forecast /> */}
+        <Nowcasting observation={observation} weatherType={weatherType} />
+        <WeatherIcon weatherType={weatherType} />
+        {/* <Forecast /> */}
       </div>
     </div>
   );
@@ -66,7 +103,7 @@ function Nowcasting(props) {
     <div className="weatherInfo">
       <div className="location">{props.observation.location || ''}</div>
       <div className="temperature">{Math.round(props.observation.temperature || 0)}<span>&#8451;</span></div>
-      <div className="description"></div>
+      <div className="description">{props.weatherType.description || ''}</div>
       <div className="windSpeed">
         <img src={windSpeed}/>
         <span>{props.observation.windSpeed} m/s</span>
@@ -78,6 +115,35 @@ function Nowcasting(props) {
     </div>
   );
 }
+
+function WeatherIcon() {
+  return (
+    <div>
+      <img src="" className="weatherIcon"/>
+    </div>
+  );
+}
+
+function yearMonthDayString(d) {
+  return Intl.DateTimeFormat('zh-TW', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  })
+  .format(d)
+  .replace(/\//g, '-');
+}
+
+function addDays(date, days) {
+  let newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate
+}
+
+function isDay(now, sunrise, sunset) {
+  return sunrise.getTime() <= now.getTime() && now.getTime() <= sunset.getTime() 
+}
+
 
 
 function App() {
